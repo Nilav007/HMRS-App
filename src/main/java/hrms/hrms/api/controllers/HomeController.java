3f035fd06b4dcffe2b0b0c4af9282481ad57696a
Anupam Nilav <anupam.nilav1121@gmail.com>
2026-01-12 16:15:33 +0530

    Day_4.9.1.1_HRMS_Portal

---
+++ src/main/java/hrms/hrms/api/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package hrms.hrms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to HRMS Application! The API is running successfully.";
+    }
+}
